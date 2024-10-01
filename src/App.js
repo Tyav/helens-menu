09ccccfda7c22be82e44b27f5b9993bc6b0d07b7
Moses Aondofa Tyav <tyav2greenz@gmail.com>
2024-10-01 12:46:49 +0100

    Refactor recipe display and add collapse functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import data from './data.json';
 
 function App() {
+  const [isCollapsed, setIsCollapsed] = useState(null);
+
+  const toggleCollapse = (num) => {
+    if (isCollapsed === num) {
+      setIsCollapsed(null);
+      return;
+    }
+
+    setIsCollapsed(num);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">Helen's Menu</div>
+      <div className="recipe-container">
+        {
+          data.map((recipe) => {
+            return (
+              <div
+                className="recipe"
+                key={recipe.id}
+                style={{ cursor: 'pointer' }}
+              >
+                <h6 className="title" onClick={() => toggleCollapse(recipe.id)} >{recipe.name} <span className='title-tag'>{recipe.tag}</span></h6>
+                {(
+                  <div className={`recipe-ingredients ${ isCollapsed === recipe.id ? "expanded": "" }`}>
+                    <p className='desc'>{recipe.description}</p>
+                    <p>Ingredients:</p>
+                    <div className="ingredient-container">
+                      {
+                        recipe.ingredients.map((ingredient, i) => {
+                          return (
+                            <div className="ingredients" key={i}>{ingredient}</div>
+                          );
+                        })
+                      }
+                    </div>
+                  </div>
+                )}
+              </div>
+            );
+          })
+        }
+
+      </div>
     </div>
   );
 }
